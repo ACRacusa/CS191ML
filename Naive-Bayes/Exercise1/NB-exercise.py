295756b17e9058a91db92a34b9083612848b3823
Aldrin Racusa <acracusa@up.edu.ph>
2019-02-23 12:00:59 +0800

    successfully tokenized the data and then stored into csv file.

--- Naive-Bayes/Exercise1/NB-exercise.py
+++ Naive-Bayes/Exercise1/NB-exercise.py
@@ -1,7 +1,129 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, especially CSV file (such as pd.read_csv)
-import os
+import email, re, os, threading, string
+import pandas as pd
+import numpy as np
+import mailparser
+import nltk
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
 
+stemming = PorterStemmer()
+stops = set(stopwords.words("english")) 
+path = 'C:/Users/Aldrin/Desktop/school_folders/CS191-ML/trec07p/'
+indexFilePath = 'full/index'
+csvFileName = 'parsedEmails.csv'
+tokenizedData = 'tokenizedData.csv'
+#Source: https://www.kaggle.com/dilip990/spam-ham-detection-using-naive-bayes-classifier
+def text_process(mess):
+    nopunc =[char for char in mess if char not in string.punctuation]
+    nopunc=''.join(nopunc)
+    return [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
 
+def cleanhtml(raw_html):
+    #cleanr = re.compile('<[^<]+?>')
+    #cleantext = re.sub(cleanr, '', raw_html)
+    #removes the unnecessary unicode characters
+    #cleantext = raw_html.encode('utf-8', 'ignore')
+    cleantext = re.sub(r'\\[Uu][a-zA-Z0-9]{4}', '', raw_html)
+    #cleantext = BeautifulSoup(cleantext) 
+    cleantext = cleanMe(cleantext)
+    return cleantext
+
+#Source: https://stackoverflow.com/questions/30565404/remove-all-style-scripts-and-html-tags-from-an-html-page/30565420
+def cleanMe(html):
+    soup = BeautifulSoup(html,"html.parser") # create a new bs4 object from the html data loaded
+    for script in soup(["script", "style"]): # remove all javascript and stylesheet code
+        script.extract()
+    # get text
+    text = soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = ' '.join(chunk for chunk in chunks if chunk)
+    return text.replace('\t','')
+
+#Source: https://stackoverflow.com/questions/20078816/replace-non-ascii-characters-with-a-single-space
+# and for the ord(): https://www.programiz.com/python-programming/methods/built-in/ord
+def deleteNonASCII(text):
+    return ''.join([i if ord(i) < 128 else '' for i in text])
+
+def deprecatedPreprocess(spamClassifier):
+   bow_transformer = CountVectorizer(analyzer=text_process).fit(spamClassifier['text'])
+   print('bow length: ',len(bow_transformer.vocabulary_))
+   message4=spamClassifier['text'][3]
+   bow4=bow_transformer.transform([message4])
+   print('bow4 ',bow4)
+   print('bow4 shape: ',bow4.shape)
+
+   messages_bow = bow_transformer.transform(spamClassifier['text'])
+   print('Shape of Sparse Matrix: ',messages_bow.shape)
+   print('Amount of non-zero occurences:',messages_bow.nnz)
+
+   tfidf_transformer=TfidfTransformer().fit(messages_bow)
+   tfidf4 = tfidf_transformer.transform(bow4)
+   print(tfidf4)
+
+   messages_tfidf=tfidf_transformer.transform(messages_bow)
+   print(messages_tfidf.shape)
+
+   spam_detect_model = MultinomialNB().fit(messages_tfidf,spamClassifier['text'])
+   print('predicted:',spam_detect_model.predict(tfidf4)[0])
+   print('expected:',spamClassifier.text[3])
+
+def stem_list(row):
+    my_list = row
+    stemmed_list = [stemming.stem(word) for word in my_list]
+    return (stemmed_list)
+def remove_stops(row):
+    my_list = row
+    meaningful_words = [w for w in my_list if not w in stops]
+    return (meaningful_words)
+
+def rejoin_words(row):
+    my_list = row
+    joined_words = ( " ".join(my_list))
+    return joined_words
 if __name__ == "__main__":
-    pass
+   spamClassifier = pd.read_csv(os.path.join(path, csvFileName), usecols=['is_spam', 'text'])
+   spamClassifier['text'] = spamClassifier['text'].astype(str)
+   #spamClassifier = spamClassifier.iloc[0:5]
+   spamClassifier['text'] = spamClassifier['text'].apply(deleteNonASCII)
+   spamClassifier['text'] = spamClassifier['text'].apply(cleanhtml)
+   #spamClassifier['text'].replace('', np.nan, inplace=True)
+   #spamClassifier['text'].dropna(how='any', inplace=True)
+   #lowercase
+   #spamClassifier['text'] = spamClassifier['text'].map(lambda x: x.lower()) 
+   spamClassifier['text'] = spamClassifier['text'].str.lower()
+   #remove punctuations
+   spamClassifier['text'] = spamClassifier['text'].str.replace('[^\w\s]', '') 
+   #replace '' values with NAN
+   spamClassifier['text'].replace('', np.nan, inplace=True)
+   #drop NAN values
+   spamClassifier['text'].dropna(how='any', inplace=True)
+
+   # tokenize the data and remove stopwords 
+   spamClassifier['text'] = spamClassifier['text'].apply(text_process)
+   #spamClassifier['text'] = spamClassifier['text'].apply(nltk.word_tokenize) 
+#    #apply stemming
+#    spamClassifier['text'] = spamClassifier['text'].apply(stem_list)
+
+#    #remove stopwords
+#    spamClassifier['text'] = spamClassifier['text'].apply(remove_stops)
+
+#    spamClassifier['text'] = spamClassifier['text'].apply(rejoin_words)
+   #nltk.download()
+   #stemmer = PorterStemmer()
+   #spamClassifier['text'] = spamClassifier['text'].apply(lambda x: [stemmer.stem(y) for y in x])  
+   
+   
+   #print(spamClassifier)
+   spamClassifier.to_csv(os.path.join(path, tokenizedData))
+   #print(spamClassifier.groupby('text').describe())
+   #Preprocess?
+
+   
--- Naive-Bayes/Exercise1/parse-email.py
+++ Naive-Bayes/Exercise1/parse-email.py
@@ -1,4 +1,4 @@
-import email, re, os, threading
+import email, re, os, threading,string
 import pandas as pd
 import numpy as np
 import mailparser
@@ -9,25 +9,6 @@ indexFilePath = 'full/index'
 csvFileName = 'parsedEmails.csv'
 exitFlag = 0
 
-# def readEmail(thread_name, email_frame):  
-#     folder = os.fsencode(path)
-
-#     for file in sorted(os.listdir(folder)):
-#         filename = os.fsdecode(file)
-#         #print(fileClass.loc[i,0])
-#         print(extract_email_from_file(path+"/"+filename))
-
-#iterate through the data frame rows 
-#Source: https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
-def sequentialReadEmail(email_frame):
-    for index, row in email_frame.iterrows():
-        print(row['email_filename'])
-        try:
-            email_frame.at[index, 'text'] = parseEmailFromFile(os.path.join(path, row['email_filename']))
-        except:
-            email_frame.at[index, 'text'] = ''
-            continue
-    return email_frame
 #parallel read email
 def readEmail(thread_name, email_frame):
     for index, row in email_frame.iterrows():
@@ -40,63 +21,97 @@ def readEmail(thread_name, email_frame):
     
     if exitFlag:
         thread_name.exit()
+ 
 
-#Preprocess the "/full/index" to convert it to csv file
+def cleanhtml(raw_html):
+    #cleanr = re.compile('<[^<]+?>')
+    #cleantext = re.sub(cleanr, '', raw_html)
+    #removes the unnecessary unicode characters
+    #cleantext = raw_html.encode('utf-8', 'ignore')
+    cleantext = re.sub(r'\\[Uu][a-zA-Z0-9]{4}', '', raw_html)
+    #cleantext = BeautifulSoup(cleantext) 
+    cleantext = cleanMe(cleantext)
+    return cleantext
+
+#Source: https://stackoverflow.com/questions/30565404/remove-all-style-scripts-and-html-tags-from-an-html-page/30565420
+def cleanMe(html):
+    soup = BeautifulSoup(html,"html.parser") # create a new bs4 object from the html data loaded
+    for script in soup(["script", "style"]): # remove all javascript and stylesheet code
+        script.extract()
+    # get text
+    text = soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = ' '.join(chunk for chunk in chunks if chunk)
+    return text.replace('\t','')
+
+#Source: https://stackoverflow.com/questions/20078816/replace-non-ascii-characters-with-a-single-space
+# and for the ord(): https://www.programiz.com/python-programming/methods/built-in/ord
+def deleteNonASCII(text):
+    return ''.join([i if ord(i) < 128 else '' for i in text])
+
+#Extract the emailfile from the given filename and then cleans the html tags
+def parseEmailFromFile(emailFile):
+    mail = mailparser.parse_from_file(emailFile)
+    #noAsciiEmailBody = deleteNonASCII(mail.body)
+    #email_body = cleanhtml(noAsciiEmailBody)
+    #email_body.replace('', np.nan, inplace=True)
+    #finalCleanEmail = email_body.replace('\n',' ').strip()
+    return mail.body
+    #return finalCleanEmail
+    
+#Preprocess the "/full/index"
 def preprocessDataset(index):
     fileClassification = pd.read_csv(index, sep=' ', names=['is_spam', 'email_filename'])
+
     fileClassification['is_spam'] = fileClassification['is_spam'].map({'spam': 1, 'ham': 0})
     fileClassification['email_filename'] = [x.replace('../', '') for x in fileClassification['email_filename']]
     fileClassification['text'] = ''
 
-    #sequentialProcessing(fileClassification)
-    parallelProcessing(fileClassification)
-    #print(fileClassification)
-
-def sequentialProcessing(fileClassification):
-    resultingFrame = sequentialReadEmail(fileClassification)
-
-    #concatinate all of the resulting frames
-    dataFrameResult = pd.concat([resultingFrame])
-    
+    dataFrameResult = parallelProcessing(fileClassification)
     #convert the result into a csv file
     dataFrameResult.to_csv(os.path.join(path, csvFileName))
+
 
 def parallelProcessing(fileClassification): 
     #split the array into 2 parts
-    #fileSubArray = np.array_split(fileClassification,5)
+    fileSubArray = np.array_split(fileClassification,5)
     
     # Create new threads
-    thread1 = myThread(1, "email-Thread-1", fileClassification)
-    # thread1 = myThread(1, "email-Thread-1", fileSubArray[0])
-    # thread2 = myThread(2, "email-Thread-2", fileSubArray[1])
-    # thread3 = myThread(3, "email-Thread-3", fileSubArray[2])
-    # thread4 = myThread(4, "email-Thread-4", fileSubArray[3])
-    # thread5 = myThread(5, "email-Thread-5", fileSubArray[4])
+    #thread1 = myThread(1, "email-Thread-1", fileClassification)
+    thread1 = myThread(1, "email-Thread-1", fileSubArray[0])
+    thread2 = myThread(2, "email-Thread-2", fileSubArray[1])
+    thread3 = myThread(3, "email-Thread-3", fileSubArray[2])
+    thread4 = myThread(4, "email-Thread-4", fileSubArray[3])
+    thread5 = myThread(5, "email-Thread-5", fileSubArray[4])
 
     # Start new Threads
     thread1.start()
-    # thread2.start()
-    # thread3.start()
-    # thread4.start()
-    # thread5.start()
+    thread2.start()
+    thread3.start()
+    thread4.start()
+    thread5.start()
 
     # Join the threads
     thread1.join()
-    # thread2.join()
-    # thread3.join()
-    # thread4.join()
-    # thread5.join()
+    thread2.join()
+    thread3.join()
+    thread4.join()
+    thread5.join()
 
 
     #merge all the data that was collected
-    # resultingFrames = [thread1.files,thread2.files,thread3.files,thread4.files,thread5.files]
-    resultingFrames = [thread1.files]
+    resultingFrames = [thread1.files,thread2.files,thread3.files,thread4.files,thread5.files]
+    #resultingFrames = [thread1.files]
     #concatinate all of the resulting frames
     dataFrameResult = pd.concat(resultingFrames)
-    
-    #convert the result into a csv file
-    dataFrameResult.to_csv(os.path.join(path, csvFileName))
+    #dataFrameResult.replace('', np.nan, inplace=True)
+    #dataFrameResult.dropna(how='any', subset=['text'], inplace=True)
     #print(dataFrameResult)
+    return dataFrameResult
 
 #Threading class in order to create a parallel processing
 #source: https://www.tutorialspoint.com/python/python_multithreading.htm
@@ -111,23 +126,22 @@ class myThread (threading.Thread):
         print ("Starting " + self.name)
         readEmail(self.name, self.files)
         print ("Exiting " + self.name)
- 
 
-#clean the html tags from the string
-#Source: https://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string
-def cleanhtml(raw_html):
-    cleanr = re.compile('<[^<]+?>')
-    cleantext = re.sub(cleanr, '', raw_html)
-    cleantext = BeautifulSoup(cleantext, "html.parser").text
-    return cleantext
-
-#Extract the emailfile from the given filename and then cleans the html tags
-def parseEmailFromFile(emailFile):
-    mail = mailparser.parse_from_file(emailFile)
-    email_body = cleanhtml(mail.body)
-    finalCleanEmail = email_body.replace('\n',' ').strip()
-    return finalCleanEmail
 
 if __name__ == "__main__":
    #readEmail(os.path.join(path, 'full/index'))
+   #read the email one by one
    preprocessDataset(os.path.join(path, indexFilePath))
+   
+#    spamClassifier = pd.read_csv(os.path.join(path, csvFileName), usecols=['is_spam', 'text'])
+#    spamClassifier['text'] = spamClassifier['text'].astype(str)
+
+#    spamClassifier['text'] = spamClassifier['text'].apply(deleteNonASCII)
+#    spamClassifier['text'] = spamClassifier['text'].apply(cleanhtml)
+
+#    spamClassifier['text'].replace('', np.nan, inplace=True)
+#    spamClassifier['text'].dropna(how='any', inplace=True)
+#    #print(spamClassifier.head(10))
+
+#    print(spamClassifier.groupby('text').describe())
+
